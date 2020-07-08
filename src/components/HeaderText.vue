2251d7940565ec078077a38420c78ab103508746
Lindsay Chapin <lnchapin@gmail.com>
2020-07-07 21:36:04 -0600

    wip header text

---
+++ src/components/HeaderText.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-container fluid>
+    <v-row>
+      <!-- <v-col cols='12' md='4'>
+        <v-slider v-model='selected' prepend-icon='mdi-pan-horizontal'
+        min='0' max='24' thumb-label></v-slider>
+      </v-col> -->
+      <v-col cols='12' md='4' offset-md='3'>
+        <v-card :elevation='selected'>
+          <v-card-text>
+            <h1 class='text-center ma-2 pa-6' style="font-size: 4em;">What is {{msg}}?</h1>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'HeaderText',
+
+  props: ['msg'],
+
+  data() {
+    return {
+      selected: 2
+    };
+  }
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class='home'>
+    <img alt='Vue logo' src='../assets/logo.png' />
+    <HeaderText msg='Welcome to Your Vue.js App' />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderText from '@/components/HeaderText.vue';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    HeaderText
   }
-}
+};
 </script>
