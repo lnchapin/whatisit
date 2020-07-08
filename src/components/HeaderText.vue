67756786b99143f9631055d798eadcfba9c5d4ba
Lindsay Chapin <lnchapin@gmail.com>
2020-07-07 23:02:56 -0600

    built out framework of Search and header text

--- src/components/HeaderText.vue
+++ src/components/HeaderText.vue
@@ -1,14 +1,14 @@
 <template>
   <v-container fluid>
     <v-row>
-      <!-- <v-col cols='12' md='4'>
-        <v-slider v-model='selected' prepend-icon='mdi-pan-horizontal'
-        min='0' max='24' thumb-label></v-slider>
-      </v-col> -->
       <v-col cols='12' md='4' offset-md='3'>
         <v-card :elevation='selected'>
           <v-card-text>
-            <h1 class='text-center ma-2 pa-6' style="font-size: 4em;">What is {{msg}}?</h1>
+            <h1
+            class='text-center ma-2 pa-6'
+            style="font-size: 3em; line-height:normal;"
+            v-text="`What is ${msg}?`">
+            </h1>
           </v-card-text>
         </v-card>
       </v-col>
@@ -22,7 +22,7 @@ export default {
 
   props: ['msg'],
 
-  data() {
+  data () {
     return {
       selected: 2
     };
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
   <div class='home'>
-    <img alt='Vue logo' src='../assets/logo.png' />
-    <HeaderText msg='Welcome to Your Vue.js App' />
+    <!-- <img alt='Vue logo' src='../assets/logo.png' /> -->
+    <HeaderText msg='Toxic Masculinity' />
+    <Search />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HeaderText from '@/components/HeaderText.vue';
+import Search from '@/components/Search.vue';
 
 export default {
   name: 'Home',
   components: {
-    HeaderText
+    HeaderText,
+    Search
   }
 };
 </script>
