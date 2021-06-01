f2fa892a9b413a5837503a17f748a43e47057d3c
Lindsay Chapin <lnchapin@gmail.com>
2021-06-01 17:39:45 -0600

    seeds

--- backend/seeds/furtherresearch.js
+++ backend/seeds/furtherresearch.js
@@ -1,13 +1,36 @@
-
-exports.seed = function(knex) {
+exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('fresearch').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('fresearch').insert([
+        {
+          id: 1,
+          topic_id: 1,
+          text: 'Further Research, 1, 1',
+          url: 'google.com'
+        },
+        {
+          id: 2,
+          topic_id: 2,
+          text: 'Further Research, 2, 2',
+          url: 'google.com'
+        },
+        {
+          id: 3,
+          topic_id: 3,
+          text: 'Further Research, 3, 3',
+          url: 'google.com'
+        },
+        {
+          id: 4,
+          topic_id: 1,
+          text: 'Further Research, 1, 4',
+          url: 'google.com'
+        }
       ]);
-    });
+    })
+    .then(function () {
+      return knex.raw('alter sequence fresearch_id_seq restart with 4')
+    })
 };
