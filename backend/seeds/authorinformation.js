cbe4c5bee7ad375f0408b6fa5630ad80d292daba
Lindsay Chapin <lnchapin@gmail.com>
2021-06-01 17:33:36 -0600

    seeds

--- backend/seeds/authorinformation.js
+++ backend/seeds/authorinformation.js
@@ -1,13 +1,39 @@
-
-exports.seed = function(knex) {
+exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('author').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('author').insert([
+        {
+          id: 1,
+          fName: 'Lindsay',
+          lName: 'Chapin',
+          email: 'lindsay@test.com',
+          password: '$2a$13$CiRpEgaSnbGd.zCscx99cuZFs3jSlQwkqEO140x9veCaP3zZ6fZlm',
+          info: 'hello',
+          url: 'google.com'
+        },
+        {
+          id: 2,
+          fName: 'Sarah',
+          lName: 'Nadler',
+          email: 'sarah@test.com',
+          password: '$2a$13$CiRpEgaSnbGd.zCscx99cuZFs3jSlQwkqEO140x9veCaP3zZ6fZlm',
+          info: 'hello',
+          url: 'google.com'
+        },
+        {
+          id: 3,
+          fName: 'Chance',
+          lName: 'Scott-Burke',
+          email: 'chance@test.com',
+          password: '$2a$13$CiRpEgaSnbGd.zCscx99cuZFs3jSlQwkqEO140x9veCaP3zZ6fZlm',
+          info: 'hello',
+          url: 'google.com'
+        }
       ]);
-    });
+    })
+    .then(function () {
+      return knex.raw('alter sequence author_id_seq restart with 4')
+    })
 };
